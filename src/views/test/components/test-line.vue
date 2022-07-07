773d2e77252fbf6e4bdf5bcba82d1201996fa0c0
ByteHero <19157720829@yeah.net>
2022-07-07 16:07:56 +0800

    add 404 page

---
+++ src/views/test/components/test-line.vue
@@ -0,0 +1,16 @@
+<!--这个是直线echarts测试，echarts入门-->
+<template>
+  <section id="lineChart">
+      
+  </section>
+</template>
+
+<script>
+export default {
+  name: "test-line"
+}
+</script>
+
+<style scoped>
+
+</style>
